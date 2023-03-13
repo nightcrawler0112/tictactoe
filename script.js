61df89a073d11ed66204de67f0ac14a52ef9efe1
Navin raj <rajnavinraj23@gmail.com>
2023-03-13 22:18:05 +0530

    game logic updated

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn ="X"
+
+
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
+}
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
+        }
+    })
+})
