54edadddb1d666fd19ee36c88b582f2b87943a3f
Navin raj <rajnavinraj23@gmail.com>
2023-03-15 22:35:49 +0530

    reset button

--- script.js
+++ script.js
@@ -56,5 +56,17 @@ Array.from(boxes).forEach(element =>{
     })
 })
 
-  //
+  //  reset button
+
+  reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
 
